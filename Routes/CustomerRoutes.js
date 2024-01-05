91e9a1718c4e76ce2725da1d56ecdda3e895121c
Vedant2379 <iit2022114@iiita.ac.in>
2024-01-05 13:55:17 +0530

    login authentication done

--- Routes/CustomerRoutes.js
+++ Routes/CustomerRoutes.js
@@ -6,5 +6,7 @@ const CustomerController=require("../controllers/CustomerController")
 
 router.post("/addcustomer", CustomerController.addCustomer)
 router.get("/allcustomer", CustomerController.getAllCustomers)
+router.post("/login", CustomerController.custmerLogin)
+
 
 module.exports = router
--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -1,4 +1,4 @@
-const Customer=require("../Modals/CustomerSchema")
+const Customer = require("../Modals/CustomerSchema")
 
 exports.addCustomer = (req, res) => {
     const customer = new Customer({
@@ -25,4 +25,19 @@ exports.getAllCustomers = (req, res) => {
         }).catch((err) => {
             res.status(500).send(err)
         });
+}
+
+exports.custmerLogin = (req, res) => {
+    Customer.findOne({
+        CustomerEmail: req.body.CustomerEmail,
+        CustomerPassword: req.body.CustomerPassword,
+    }).then((cust) => {
+        if (cust) {
+            res.status(200).json({ success: true, data: cust })
+        } else {
+            res.status(200).json({ success: false, data: {} })
+        }
+    }).catch((err) => {
+        res.status(500).send(err)
+    });
 }
